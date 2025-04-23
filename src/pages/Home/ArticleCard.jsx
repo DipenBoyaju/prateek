8b8ca21451dd1ddf2adb2b932b5876af02a85fe6
Dipen <dipendraboyaju222@gmail.com>
2025-04-23 10:48:55 +0545

    chnage

--- src/pages/Home/ArticleCard.jsx
+++ src/pages/Home/ArticleCard.jsx
@@ -2,7 +2,7 @@ const ArticleCard = () => {
   return (
     <div className="cursor-pointer hover:bg-zinc-900/20 py-4">
       <div className="w-full h-52 bg-zinc-800"></div>
-      <h3 className="mt-6 text-3xl px-2">Why Consistent Brand Identity is Key to Business Success</h3>
+      <h3 className="mt-6 text-lg md:text-3xl px-2">Why Consistent Brand Identity is Key to Business Success</h3>
     </div>
   )
 }
