5fe0e5558cfb40d225433be4ca9f85db75865bea
Dipen <dipendraboyaju222@gmail.com>
2025-04-23 11:27:00 +0545

    change

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,24 +2,28 @@ import { CiLinkedin } from "react-icons/ci";
 import Navbar from "../components/Navbar"
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import { SlSocialFacebook } from "react-icons/sl";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md border-b py-5 border-white/10">
+    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md border-b py-5 border-white/10 px-4 md:px-0">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <div className="">
             <p className="uppercase font-ubuntu font-semibold tracking-wider text-secondary text-lg">Prateek</p>
           </div>
-          <div className="">
+          <div className="absolute md:relative">
             <Navbar />
           </div>
-          <div className="">
+          <div className="hidden md:inline-block">
             <ul className="flex items gap-3 items-center border-[1px] border-white/20 rounded-full px-6 py-1 text-white/70">
               <li><FaFacebookF size={16} /></li>
               <li><FaInstagram size={20} /></li>
               <li><CiLinkedin size={20} /></li>
             </ul>
+          </div>
+          <div className="md:hidden">
+            <HiOutlineMenuAlt3 className="text-primary text-3xl" />
           </div>
         </div>
       </div>
