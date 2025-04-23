385895ab9e6f69197a197de889f3331b6183fbf5
Dipen <dipendraboyaju222@gmail.com>
2025-04-23 11:40:10 +0545

    change

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,13 +6,13 @@ import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md border-b py-5 border-white/10 px-4 md:px-0">
+    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md border-b py-3 md:py-5 border-white/10 px-4 md:px-0">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <div className="">
             <p className="uppercase font-ubuntu font-semibold tracking-wider text-secondary text-lg">Prateek</p>
           </div>
-          <div className="absolute md:relative">
+          <div className="absolute md:relative top-16 md:top-0 backdrop-blur-2xl pt-5 px-4 md:px-0 md:pt-0 w-full md:w-auto left-0">
             <Navbar />
           </div>
           <div className="hidden md:inline-block">
@@ -22,8 +22,8 @@ const Header = () => {
               <li><CiLinkedin size={20} /></li>
             </ul>
           </div>
-          <div className="md:hidden">
-            <HiOutlineMenuAlt3 className="text-primary text-3xl" />
+          <div className="md:hidden bg-zinc-950 border border-white/30 p-2 rounded-3xl">
+            <HiOutlineMenuAlt3 className="text-white text-xl" />
           </div>
         </div>
       </div>
