4735d85a5bb210547c8af3b938f939ae27277a8c
Dipen <dipendraboyaju222@gmail.com>
2025-04-23 10:38:29 +0545

    change

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -7,7 +7,7 @@ const NewsLetter = () => {
           <h2 className=" text-3xl md:text-5xl text-primary font-ubuntu tracking-wider mt-2">Subscribe to our Newsletter!</h2>
           <div className="bg-zinc-900 w-fit mx-auto mt-8 rounded-full">
             <form action="" className="p-2 md:p-3 gap-2 flex items-center ">
-              <input type="email" className="w-full md:w-[50vw] h-[40px] md:h-[50px] rounded-full focus:outline-none pl-4 font-ubuntu text-white/60 text-base md:text-xl" placeholder="Enter your mail..." required />
+              <input type="email" className="w-full md:w-[40vw] h-[40px] md:h-[50px] rounded-full focus:outline-none pl-4 font-ubuntu text-white/60 text-base md:text-xl" placeholder="Enter your mail..." required />
               <button className="bg-secondary text-darkBlack text-xs md:text-base font-geist py-3 md:py-4 px-6 md:px-8 rounded-full hover:bg-secondary/80 cursor-pointer transition-colors ease-in-out duration-300">Subscribe</button>
             </form>
           </div>
--- src/pages/Home/Articles.jsx
+++ src/pages/Home/Articles.jsx
@@ -4,15 +4,15 @@ import ArticleCard from "./ArticleCard"
 
 const Articles = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 px-4 md:px-0">
       <div className="container mx-auto">
-        <h1 className="font-ubuntu tracking-wider text-5xl text-primary leading-14">Our Latest <br />Articles</h1>
-        <div className="flex gap-72 items-center">
+        <h1 className="font-ubuntu tracking-wider text-4xl md:text-5xl text-primary leading-14">Our Latest <br className="hidden md:block" />Articles</h1>
+        <div className="flex flex-col md:flex-row md:gap-72 items-start md:items-center">
           <p className="text-white/60 pt-4">Explore the insights and trends shaping our industry</p>
           <Link to={'/blog'} className="flex items-center gap-2 text-lg text-secondary/80">See all articles <MoveRight /></Link>
         </div>
 
-        <div className="grid grid-cols-3 gap-10 mt-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-10 mt-8">
           <ArticleCard />
           <ArticleCard />
           <ArticleCard />
